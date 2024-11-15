b267230f00a828cd02d71fd7d299c69b3fab22fd
mj123-mohit <mj123.mohit@gmail.com>
2024-11-15 12:22:39 +0545

    modified:   taskmanager/tasks/admin.py 	new file:   taskmanager/tasks/migrations/0001_initial.py 	new file:   taskmanager/tasks/migrations/0002_auto_20241114_1025.py 	new file:   taskmanager/tasks/migrations/0003_epic_sprint_alter_task_table_comment_and_more.py 	modified:   taskmanager/tasks/models.py

--- taskmanager/tasks/admin.py
+++ taskmanager/tasks/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from tasks.models import Task, Epic, Sprint
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "status", "owner", "created_at", "updated_at")
+    list_filter = ("status",)
+    actions = ['mark_archived']
+    def mark_archived(self, request, queryset):
+        queryset.update(status='ARCHIVED')
+    mark_archived.short_description = 'Mark selected tasks as archived'
+    def has_change_permission(self, request, obj=None):
+        if request.user.has_perm('tasks.change_task'):
+            return True
+        return False
+    def has_add_permission(self, request):
+        if request.user.has_perm('tasks.add_task'):
+            return True
+        return False
+    def has_delete_permission(self, request, obj=None):
+        if request.user.has_perm('tasks.delete_task'):
+            return True
+        return False
+    
+class EpicAdmin(admin.ModelAdmin):
+    pass
+
+class SprintAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Epic, EpicAdmin)
+admin.site.register(Sprint, SprintAdmin)
